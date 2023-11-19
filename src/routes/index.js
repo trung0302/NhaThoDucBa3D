4c5d3ea6068fdec3967ae731f05cfca12475bd60
LAPTOP-TRUNG <thanhtrungvnger@gmail.com>
2023-11-19 22:50:19 +0700

    add change password page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Register from "../Pages/Register";
 import NotFound from "../Pages/NotFound";
 import Model3D from "../Pages/Model3D";
 import Contact from "../Pages/Contact";
+import ChangePassword from "../Pages/ChangePassword";
 
 const publicRoutes = [
     { path: "/", component: <Home /> },
@@ -12,6 +13,7 @@ const publicRoutes = [
     { path: "/login", component: <Login />, layout: null },
     { path: "/register", component: <Register />, layout: null },
     { path: "/contact", component: <Contact />},
+    { path: "/change-password", component: <ChangePassword />},
 ];
 
 export { publicRoutes };
